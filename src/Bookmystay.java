c4f39a6f2fe43a008bbaff8f9e7cb2f9a99bc76d
Priyanshu710-ui <nanu66292@gmail.com>
2026-04-10 09:54:37 +0530

    Update BookMyStay.java

---
+++ src/Bookmystay.java
@@ -0,0 +1,12 @@
+
+public class Bookmystay{
+
+    public static void main(String[] args) {
+
+        System.out.println("      Welcome to Book My Stay       ");
+        System.out.println("   Hotel Booking Management System  ");
+        System.out.println("          Version: 1.0              ");
+        System.out.println("Application started successfully.");
+        System.out.println("Thank you for using Book My Stay!");
+    }
+}
